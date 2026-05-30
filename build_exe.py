d94e61155e252931aff0d68d6805abca855b492f
swkim3118-svg <swkim3118@gmail.com>
2026-05-30 21:28:11 +0900

    Point overlay to new EC2 API

--- build_exe.py
+++ build_exe.py
@@ -38,7 +38,7 @@ def build():
             "1. Unzip MirrorDungeonTracker.zip first.\n"
             "2. Keep all files in the extracted MirrorDungeonTracker folder.\n"
             "3. Run MirrorDungeonTracker.exe inside that folder.\n"
-            "4. The default server is http://3.87.73.74:8080.\n"
+            "4. The default server is http://23.22.200.94:8080.\n"
             "5. For local development, set MD_SERVER_URL before running the app.\n"
         )
 
